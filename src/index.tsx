c9fce915fcae71ad918dff7c9a47d7d3266a0a79
colanyanya <colanyanya@163.com>
2022-06-02 08:35:17 +0800

    first commit

---
+++ src/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Text, View } from "react-native";
+import {test} from '@/pages/test/test/index'
+
+test()
+
+const App = ()=>{
+    return(
+        <View>
+            <Text>app</Text>
+        </View>
+    )
+}
+
+export default App
